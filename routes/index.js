b85b919691524e6dc0ec69de12e211153bae30a1
Sachin Shingade <39953508+sachinshingade@users.noreply.github.com>
2019-10-16 10:34:18 +0530

    added post route

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post('/update/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 
 module.exports = router;
